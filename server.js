d268e5106390bf1e10360de63ed254998f752058
Dayne Daylong <dalexad@gmail.com>
2019-12-11 14:25:56 -0800

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3077;
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
